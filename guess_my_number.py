5e0aca237d7df7035abcbc645310db566797d332
Gabriel Lima <gabrieldesouzalima@gmail.com>
2021-05-13 20:45:43 -0300

    Melhora do Result.

--- guess_my_number.py
+++ guess_my_number.py
@@ -1,12 +1,14 @@
 import random
 answer = random.randint(1, 10)
-guess = int(input('What value do you think is the real one?'))
-print(answer)
+guess = int(
+    input('What value do you think is the real? \nEnter values between 1 and 10.? \n'))
 
 if guess < answer:
-    result = "Oops! Your guess was too low."
+    result = "Oops! Your guess was too low. \nThe number thought was: {}".format(
+        answer)
 elif guess > answer:
-    result = "Oops! Your guess was too high."
+    result = "Oops! Your guess was too high. \nThe number thought was: {}".format(
+        answer)
 elif guess == answer:
     result = "Nice! Your guess matched the answer!"
 

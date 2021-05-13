9d5fcfbeb8a0ccca34c930f55da9aad0e2f1561f
Gabriel Lima <gabrieldesouzalima@gmail.com>
2021-05-13 19:58:53 -0300

    Input e Random Value.

--- guess_my_number.py
+++ guess_my_number.py
@@ -1,14 +1,13 @@
 import random
 answer = random.randint(1, 10)
 guess = int(input('What value do you think is the real one?'))
-
 print(answer)
 
 if guess < answer:
-    result = "Oops!  Your guess was too low."
+    result = "Oops! Your guess was too low."
 elif guess > answer:
-    result = "Oops!  Your guess was too high."
+    result = "Oops! Your guess was too high."
 elif guess == answer:
-    result = "Nice!  Your guess matched the answer!"
+    result = "Nice! Your guess matched the answer!"
 
 print(result)

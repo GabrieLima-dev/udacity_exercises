10a720a7ad820b51b61fd14b3c5e961e80f0f687
Gabriel Lima <gabrieldesouzalima@gmail.com>
2021-05-14 11:16:28 -0300

    Validação Número Correto

--- guess_my_number.py
+++ guess_my_number.py
@@ -3,13 +3,16 @@ answer = random.randint(1, 10)
 guess = int(
     input('What value do you think is the real? \nEnter values between 1 and 10.? \n'))
 
-if guess < answer:
-    result = "Oops! Your guess was too low. \nThe number thought was: {}".format(
+if guess > 10:
+    result = "Oh no, you entered an incorrect number. \nEnter a number between 1 and 10."
+else:
+    if guess < answer:
+        result = "Oops! Your guess was too low. \nThe number thought was: {}".format(
         answer)
-elif guess > answer:
-    result = "Oops! Your guess was too high. \nThe number thought was: {}".format(
+    elif guess > answer:
+        result = "Oops! Your guess was too high. \nThe number thought was: {}".format(
         answer)
-elif guess == answer:
-    result = "Nice! Your guess matched the answer!"
+    elif guess == answer:
+        result = "Nice! Your guess matched the answer!"
 
 print(result)
---
+++ which_prize2.py
@@ -0,0 +1,16 @@
+points = int(input())
+prize = None
+
+if points <= 50:
+    prize = "wooden rabbit"
+elif 151 <= points <= 180:
+    prize = "wafer-thin mint"
+elif points >= 181:
+    prize = "penguin"
+
+if prize:
+    result = "Congratulations! You won a {}!".format(prize)
+else:
+    result = "Oh dear, no prize this time."
+
+print(result)
